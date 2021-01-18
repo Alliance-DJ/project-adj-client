efd88d0432ca3b2e1fa1ba0d24e463fa3ecb9468
ParkDongUk <phk09242@gmail.com>
2021-01-18 18:23:18 +0900

    Add DontDestroyMonoSingleton

--- Assets/Scripts/Singleton/MonoSingleton.cs
+++ Assets/Scripts/Singleton/MonoSingleton.cs
@@ -15,6 +15,38 @@ public class MonoSingleton<T> : MonoBehaviour where T : Component
 
                 var obj = type.gameObject;
                 obj.name = $"{typeof(T).Name}(Singleton)";
+
+                if (types.Length > 1)
+                    Debug.LogError($"There is more than one {typeof(T).Name} in the scene.");
+            }
+
+            if (instance == null)
+            {
+                GameObject obj = new GameObject($"{typeof(T).Name}(Singleton)");
+                instance = obj.AddComponent<T>();
+            }
+
+            return instance;
+        });
+
+    public static bool IsSingletonCreated => _instance.IsValueCreated;
+    public static T Instance => _instance.Value;
+}
+
+public class DontDestroyMonoSingleton<T> : MonoBehaviour where T : Component
+{
+    private static readonly Lazy<T> _instance =
+        new Lazy<T>(() =>
+        {
+            T instance = null;
+            var types = FindObjectsOfType<T>();
+            if (types.Length > 0)
+            {
+                var type = types[0];
+                instance = type;
+
+                var obj = type.gameObject;
+                obj.name = $"{typeof(T).Name}(Singleton)";
                 DontDestroyOnLoad(obj);
 
                 if (types.Length > 1)
