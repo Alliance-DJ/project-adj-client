f066161e9dec56e965bdafafd39fd150d7d429c5
Park Dong Uk <phk09242@aiaracorp.com>
2021-06-09 18:11:28 +0900

    Add Editor Namespace

--- Assets/Scripts/Mapper/Editor/DataMapperEditor.cs
+++ Assets/Scripts/Mapper/Editor/DataMapperEditor.cs
@@ -1,73 +1,72 @@
-#if UNITY_EDITOR
-
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEditor;
 
-[CustomEditor(typeof(DataMapper))]
-public class DataMapperEditor : Editor
+namespace Editor
 {
-    private const string None = "None";
-
-    private string searchKey;
-    private int currentIndex = -1;
-
-    private DataMapper mapper;
-
-    private List<string> dataTypeNames;
-
-    private void OnEnable()
+    [CustomEditor(typeof(DataMapper))]
+    public class DataMapperEditor : UnityEditor.Editor
     {
-        mapper = target as DataMapper;
-        if (mapper == null) return;
+        private const string None = "None";
 
-        dataTypeNames = new List<string>();
+        private string searchKey;
+        private int currentIndex = -1;
 
-        var dataTypes = TypeCache.GetSubClasses<BaseData>();
-        if (dataTypes != null)
+        private DataMapper mapper;
+
+        private List<string> dataTypeNames;
+
+        private void OnEnable()
         {
-            Parallel.ForEach(dataTypes, (type) => { dataTypeNames.Add(type.Name); });
-        }
-    }
+            mapper = target as DataMapper;
+            if (mapper == null) return;
 
-    public override void OnInspectorGUI()
-    {
-        if (mapper == null) return;
+            dataTypeNames = new List<string>();
 
-        var types = new List<string>(dataTypeNames);
-        types.Sort();
-
-        searchKey = EditorGUILayout.TextField("Search Data : ", searchKey);
-        var searching = !string.IsNullOrEmpty(searchKey);
-        if (searching)
-            types.RemoveAll(n => !n.ToLower().Contains(searchKey.ToLower()));
-
-        types.Insert(0, None);
-
-        var currentTypeName = !string.IsNullOrEmpty(mapper.inspectorDataType) ? mapper.inspectorDataType : None;
-        EditorGUI.BeginChangeCheck();
-        currentIndex = EditorGUILayout.Popup(types.FindIndex(type => type == currentTypeName), types.ToArray());
-
-        if (EditorGUI.EndChangeCheck())
-        {
-            Undo.RecordObject(mapper, "Change DataMapper Data");
-
-            var pMappers = mapper.GetPropertyMappers();
-            if (pMappers != null && pMappers.Count > 0)
+            var dataTypes = TypeCache.GetSubClasses<BaseData>();
+            if (dataTypes != null)
             {
-                Undo.RecordObjects(pMappers.ToArray<Object>(), "Reset PropertyMapper Data");
-                Parallel.ForEach(pMappers, (pMapper) => { pMapper.ResetPropertyMapper(); });
+                Parallel.ForEach(dataTypes, (type) => { dataTypeNames.Add(type.Name); });
+            }
+        }
+
+        public override void OnInspectorGUI()
+        {
+            if (mapper == null) return;
+
+            var types = new List<string>(dataTypeNames);
+            types.Sort();
+
+            searchKey = EditorGUILayout.TextField("Search Data : ", searchKey);
+            var searching = !string.IsNullOrEmpty(searchKey);
+            if (searching)
+                types.RemoveAll(n => !n.ToLower().Contains(searchKey.ToLower()));
+
+            types.Insert(0, None);
+
+            var currentTypeName = !string.IsNullOrEmpty(mapper.inspectorDataType) ? mapper.inspectorDataType : None;
+            EditorGUI.BeginChangeCheck();
+            currentIndex = EditorGUILayout.Popup(types.FindIndex(type => type == currentTypeName), types.ToArray());
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RecordObject(mapper, "Change DataMapper Data");
+
+                var pMappers = mapper.GetPropertyMappers();
+                if (pMappers != null && pMappers.Count > 0)
+                {
+                    Undo.RecordObjects(pMappers.ToArray<Object>(), "Reset PropertyMapper Data");
+                    Parallel.ForEach(pMappers, (pMapper) => { pMapper.ResetPropertyMapper(); });
+                }
+
+                if (currentIndex >= 0)
+                    mapper.inspectorDataType = types[currentIndex] != None ? types[currentIndex] : null;
             }
 
-            if (currentIndex >= 0)
-                mapper.inspectorDataType = types[currentIndex] != None ? types[currentIndex] : null;
+            EditorGUILayout.LabelField("Selected Data Class : ",
+                !string.IsNullOrEmpty(mapper.inspectorDataType) ? mapper.inspectorDataType : None);
         }
-
-        EditorGUILayout.LabelField("Selected Data Class : ",
-            !string.IsNullOrEmpty(mapper.inspectorDataType) ? mapper.inspectorDataType : None);
     }
 }
-
-#endif
--- Assets/Scripts/Mapper/Editor/PropertyMapperEditor.cs
+++ Assets/Scripts/Mapper/Editor/PropertyMapperEditor.cs
@@ -1,173 +1,172 @@
-#if UNITY_EDITOR
-
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEditor;
 using UnityEngine;
 
-[CustomEditor(typeof(PropertyMapper), true)]
-public class PropertyMapperEditor : Editor
+namespace Editor
 {
-    private const string None = "None";
-
-    private string defaultValue;
-    private string format;
-    private string searchKey;
-    private string subSearchKey;
-    private int currentIndex = -1;
-    private int currentSubIndex = -1;
-
-    private PropertyMapper mapper;
-
-    private List<string> names;
-    private Dictionary<string, Type> returnTypeDic;
-
-    private void OnEnable()
+    [CustomEditor(typeof(PropertyMapper), true)]
+    public class PropertyMapperEditor : UnityEditor.Editor
     {
-        mapper = target as PropertyMapper;
-        if (mapper == null) return;
+        private const string None = "None";
 
-        names = new List<string>();
-        returnTypeDic = new Dictionary<string, Type>();
+        private string defaultValue;
+        private string format;
+        private string searchKey;
+        private string subSearchKey;
+        private int currentIndex = -1;
+        private int currentSubIndex = -1;
 
-        var type = mapper.GetDataMapperDataType();
-        if (type == null) return;
+        private PropertyMapper mapper;
 
-        var fields = TypeCache.GetFields(type);
-        if (fields != null)
+        private List<string> names;
+        private Dictionary<string, Type> returnTypeDic;
+
+        private void OnEnable()
         {
-            Parallel.ForEach(fields, (field) =>
+            mapper = target as PropertyMapper;
+            if (mapper == null) return;
+
+            names = new List<string>();
+            returnTypeDic = new Dictionary<string, Type>();
+
+            var type = mapper.GetDataMapperDataType();
+            if (type == null) return;
+
+            var fields = TypeCache.GetFields(type);
+            if (fields != null)
             {
-                var fName = field.Name;
-                names.Add(fName);
-            });
-        }
+                Parallel.ForEach(fields, (field) =>
+                {
+                    var fName = field.Name;
+                    names.Add(fName);
+                });
+            }
 
-        var properties = TypeCache.GetProperties(type);
-        if (properties != null)
-        {
-            Parallel.ForEach(properties, (property) =>
+            var properties = TypeCache.GetProperties(type);
+            if (properties != null)
             {
-                var pName = property.Name;
-                names.Add(pName);
-                returnTypeDic[pName] = property.GetGetMethod().ReturnType;
-            });
-        }
-    }
-
-    public override void OnInspectorGUI()
-    {
-        if (mapper == null) return;
-
-        EditorGUI.BeginChangeCheck();
-        defaultValue = EditorGUILayout.TextField("Default Value : ", mapper.defaultValue);
-
-        if (EditorGUI.EndChangeCheck())
-        {
-            Undo.RecordObject(mapper, "Change PropertyMapper Default Value");
-            mapper.defaultValue = defaultValue;
+                Parallel.ForEach(properties, (property) =>
+                {
+                    var pName = property.Name;
+                    names.Add(pName);
+                    returnTypeDic[pName] = property.GetGetMethod().ReturnType;
+                });
+            }
         }
 
-        EditorGUILayout.Space();
-
-        EditorGUILayout.LabelField("Format");
-
-        EditorGUI.BeginChangeCheck();
-        format = EditorGUILayout.TextArea(mapper.format, GUILayout.MinHeight(80f));
-
-        if (EditorGUI.EndChangeCheck())
+        public override void OnInspectorGUI()
         {
-            Undo.RecordObject(mapper, "Change PropertyMapper Format");
-            mapper.format = format;
-        }
+            if (mapper == null) return;
 
-        EditorGUILayout.Space();
+            EditorGUI.BeginChangeCheck();
+            defaultValue = EditorGUILayout.TextField("Default Value : ", mapper.defaultValue);
 
-        var tempNames = new List<string>(names);
-        tempNames.Sort();
-
-        searchKey = EditorGUILayout.TextField("Search Property: ", searchKey);
-        var searching = !string.IsNullOrEmpty(searchKey);
-        if (searching)
-            tempNames.RemoveAll(n => !n.ToLower().Contains(searchKey.ToLower()));
-
-        tempNames.Insert(0, None);
-
-        var currentTypeName = !string.IsNullOrEmpty(mapper.propertyName) ? mapper.propertyName : None;
-        EditorGUI.BeginChangeCheck();
-        currentIndex = EditorGUILayout.Popup(tempNames.FindIndex(t => t == currentTypeName), tempNames.ToArray());
-
-        if (EditorGUI.EndChangeCheck())
-        {
-            Undo.RecordObject(mapper, "Change PropertyMapper PropertyData");
-            if (currentIndex >= 0)
-                mapper.propertyName = tempNames[currentIndex] != None ? tempNames[currentIndex] : null;
-        }
-
-        EditorGUILayout.LabelField("Property Name : ",
-            !string.IsNullOrEmpty(mapper.propertyName) ? mapper.propertyName : None);
-
-        string propertyName = mapper.propertyName;
-        if (!string.IsNullOrEmpty(propertyName) &&
-            returnTypeDic != null && returnTypeDic.TryGetValue(propertyName, out var type) &&
-            type.BaseType == typeof(BaseData))
-        {
-            SetSubProperties(type);
-        }
-    }
-
-    private void SetSubProperties(Type type)
-    {
-        if (mapper == null || type == null) return;
-
-        EditorGUILayout.Space();
-
-        var tempNames = new List<string>();
-        var fields = TypeCache.GetFields(type);
-        if (fields != null)
-        {
-            Parallel.ForEach(fields, (field) =>
+            if (EditorGUI.EndChangeCheck())
             {
-                var fName = field.Name;
-                tempNames.Add(fName);
-            });
-        }
+                Undo.RecordObject(mapper, "Change PropertyMapper Default Value");
+                mapper.defaultValue = defaultValue;
+            }
 
-        var properties = TypeCache.GetProperties(type);
-        if (properties != null)
-        {
-            Parallel.ForEach(properties, (property) =>
+            EditorGUILayout.Space();
+
+            EditorGUILayout.LabelField("Format");
+
+            EditorGUI.BeginChangeCheck();
+            format = EditorGUILayout.TextArea(mapper.format, GUILayout.MinHeight(80f));
+
+            if (EditorGUI.EndChangeCheck())
             {
-                var pName = property.Name;
-                tempNames.Add(pName);
-            });
+                Undo.RecordObject(mapper, "Change PropertyMapper Format");
+                mapper.format = format;
+            }
+
+            EditorGUILayout.Space();
+
+            var tempNames = new List<string>(names);
+            tempNames.Sort();
+
+            searchKey = EditorGUILayout.TextField("Search Property: ", searchKey);
+            var searching = !string.IsNullOrEmpty(searchKey);
+            if (searching)
+                tempNames.RemoveAll(n => !n.ToLower().Contains(searchKey.ToLower()));
+
+            tempNames.Insert(0, None);
+
+            var currentTypeName = !string.IsNullOrEmpty(mapper.propertyName) ? mapper.propertyName : None;
+            EditorGUI.BeginChangeCheck();
+            currentIndex = EditorGUILayout.Popup(tempNames.FindIndex(t => t == currentTypeName), tempNames.ToArray());
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RecordObject(mapper, "Change PropertyMapper PropertyData");
+                if (currentIndex >= 0)
+                    mapper.propertyName = tempNames[currentIndex] != None ? tempNames[currentIndex] : null;
+            }
+
+            EditorGUILayout.LabelField("Property Name : ",
+                !string.IsNullOrEmpty(mapper.propertyName) ? mapper.propertyName : None);
+
+            string propertyName = mapper.propertyName;
+            if (!string.IsNullOrEmpty(propertyName) &&
+                returnTypeDic != null && returnTypeDic.TryGetValue(propertyName, out var type) &&
+                type.BaseType == typeof(BaseData))
+            {
+                SetSubProperties(type);
+            }
         }
 
-        tempNames.Sort();
-
-        subSearchKey = EditorGUILayout.TextField("Search Sub : ", subSearchKey);
-        var searching = !string.IsNullOrEmpty(subSearchKey);
-        if (searching)
-            tempNames.RemoveAll(n => !n.ToLower().Contains(subSearchKey.ToLower()));
-
-        tempNames.Insert(0, None);
-
-        var currentTypeName = !string.IsNullOrEmpty(mapper.subPropertyName) ? mapper.subPropertyName : None;
-        EditorGUI.BeginChangeCheck();
-        currentSubIndex =
-            EditorGUILayout.Popup(tempNames.FindIndex(t => t == currentTypeName), tempNames.ToArray());
-
-        if (EditorGUI.EndChangeCheck())
+        private void SetSubProperties(Type type)
         {
-            Undo.RecordObject(mapper, "Change PropertyMapper SubPropertyData");
-            if (currentSubIndex >= 0)
-                mapper.subPropertyName = tempNames[currentSubIndex] != None ? tempNames[currentSubIndex] : null;
-        }
+            if (mapper == null || type == null) return;
 
-        EditorGUILayout.LabelField("Sub Property Name : ",
-            !string.IsNullOrEmpty(mapper.subPropertyName) ? mapper.subPropertyName : None);
+            EditorGUILayout.Space();
+
+            var tempNames = new List<string>();
+            var fields = TypeCache.GetFields(type);
+            if (fields != null)
+            {
+                Parallel.ForEach(fields, (field) =>
+                {
+                    var fName = field.Name;
+                    tempNames.Add(fName);
+                });
+            }
+
+            var properties = TypeCache.GetProperties(type);
+            if (properties != null)
+            {
+                Parallel.ForEach(properties, (property) =>
+                {
+                    var pName = property.Name;
+                    tempNames.Add(pName);
+                });
+            }
+
+            tempNames.Sort();
+
+            subSearchKey = EditorGUILayout.TextField("Search Sub : ", subSearchKey);
+            var searching = !string.IsNullOrEmpty(subSearchKey);
+            if (searching)
+                tempNames.RemoveAll(n => !n.ToLower().Contains(subSearchKey.ToLower()));
+
+            tempNames.Insert(0, None);
+
+            var currentTypeName = !string.IsNullOrEmpty(mapper.subPropertyName) ? mapper.subPropertyName : None;
+            EditorGUI.BeginChangeCheck();
+            currentSubIndex =
+                EditorGUILayout.Popup(tempNames.FindIndex(t => t == currentTypeName), tempNames.ToArray());
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RecordObject(mapper, "Change PropertyMapper SubPropertyData");
+                if (currentSubIndex >= 0)
+                    mapper.subPropertyName = tempNames[currentSubIndex] != None ? tempNames[currentSubIndex] : null;
+            }
+
+            EditorGUILayout.LabelField("Sub Property Name : ",
+                !string.IsNullOrEmpty(mapper.subPropertyName) ? mapper.subPropertyName : None);
+        }
     }
 }
-
-#endif
