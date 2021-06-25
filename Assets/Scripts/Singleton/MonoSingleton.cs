1aa5a93fd310e9060286df3cae50f027f452d342
Park Dong Uk <phk09242@aiaracorp.com>
2021-06-25 14:11:22 +0900

    Change MonoSingleton

--- Assets/Scripts/Singleton/MonoSingleton.cs
+++ Assets/Scripts/Singleton/MonoSingleton.cs
@@ -3,30 +3,6 @@ using UnityEngine;
 
 public class MonoSingleton<T> : MonoBehaviour where T : Component
 {
-    private static Lazy<T> instance = new Lazy<T>(FindInstance);
-
-    public static T Instance => instance.Value;
-
-    protected void OnDestroy()
-    {
-        if (!instance.IsValueCreated) return;
-
-        instance = new Lazy<T>(FindInstance);
-    }
-
-    private static T FindInstance()
-    {
-        var instance = FindObjectOfType<T>();
-        if (!instance.IsValid())
-        {
-            Debug.LogWarning($"<color=orange>Not Found MonoSingleton : <b>{typeof(T)}</b></color>");
-        }
-        return instance;
-    }
-}
-
-public class DontDestroyMonoSingleton<T> : MonoBehaviour where T : Component
-{
     private static T instance;
     private static readonly object lockObj = new object();
 
@@ -41,16 +17,13 @@ public class DontDestroyMonoSingleton<T> : MonoBehaviour where T : Component
                     if (!instance.IsValid())
                     {
                         instance = FindObjectOfType<T>();
-                        if (!instance.IsValid())
-                        {
-                            var obj = new GameObject($"{typeof(T).Name}(Singleton)");
-                            instance = obj.AddComponent<T>();
-                            DontDestroyOnLoad(obj);
-
-                            Debug.LogWarning($"<color=orange>Not Found Singleton, Create Dont Destroy Singleton : <b>{typeof(T)}</b></color>", obj);
-                        }
                     }
                 }
+            }
+
+            if (!instance.IsValid())
+            {
+                Debug.LogWarning($"<color=red>Not Found MonoSingleton : <b>{typeof(T)}</b></color>");
             }
 
             return instance;
@@ -59,9 +32,16 @@ public class DontDestroyMonoSingleton<T> : MonoBehaviour where T : Component
 
     protected virtual void Awake()
     {
-        if (instance.IsValid() && instance != this)
+        if (!instance.IsValid())
         {
-            Debug.LogWarning($"<color=orange>Duplicate Dont Destroy Singleton : <b>{typeof(T)}</b></color>");
+            instance = FindInstance();
+
+            if (!instance.IsValid())
+                Debug.LogWarning($"<color=red>Not Found Dont Destroy MonoSingleton : <b>{typeof(T)}</b></color>");
+        }
+        else if (instance != this)
+        {
+            Debug.LogWarning($"<color=orange>Duplicate MonoSingleton : <b>{typeof(T)}</b></color>");
 
             var components = gameObject.GetComponents<Component>();
             if (components.Length <= 2)
@@ -69,22 +49,91 @@ public class DontDestroyMonoSingleton<T> : MonoBehaviour where T : Component
             else
                 Destroy(this);
         }
-        else
+    }
+
+    protected virtual void OnDestroy()
+    {
+        if (instance != this) return;
+
+        instance = null;
+    }
+
+    private T FindInstance()
+    {
+        return this as T;
+    }
+}
+
+public class DontDestroyMonoSingleton<T> : MonoBehaviour where T : Component
+{
+    private static T instance;
+    private static readonly object lockObj = new object();
+
+    private bool shuttingDown;
+
+    public static T Instance
+    {
+        get
+        {
+            if (!instance.IsValid())
+            {
+                lock (lockObj)
+                {
+                    if (!instance.IsValid())
+                    {
+                        instance = FindObjectOfType<T>();
+                    }
+                }
+            }
+
+            if (!instance.IsValid())
+            {
+                Debug.LogWarning($"<color=red>Not Found Dont Destroy MonoSingleton : <b>{typeof(T)}</b></color>");
+            }
+
+            return instance;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        if (!instance.IsValid())
         {
             instance = FindInstance();
 
             if (instance.IsValid())
                 DontDestroyOnLoad(gameObject);
+            else
+                Debug.LogWarning($"<color=red>Not Found Dont Destroy MonoSingleton : <b>{typeof(T)}</b></color>");
         }
+        else if (instance != this)
+        {
+            Debug.LogWarning($"<color=orange>Duplicate Dont Destroy MonoSingleton : <b>{typeof(T)}</b></color>");
+
+            var components = gameObject.GetComponents<Component>();
+            if (components.Length <= 2)
+                Destroy(gameObject);
+            else
+                Destroy(this);
+        }
+
+    }
+
+    private void OnApplicationQuit()
+    {
+        shuttingDown = true;
     }
 
     protected virtual void OnDestroy()
     {
-        if (instance == this)
+        if (instance != this) return;
+
+        if (!shuttingDown)
         {
-            Debug.LogWarning($"<color=red>Destroy Dont Destroy Singleton : <b>{typeof(T)}</b></color>");
-            instance = null;
+            Debug.LogWarning($"<color=red>Destroy Dont Destroy MonoSingleton : <b>{typeof(T)}</b></color>");
         }
+
+        instance = null;
     }
 
     private T FindInstance()
